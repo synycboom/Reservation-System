94cc469b2211b1e6f61259a578d20f64dae52904
pluanant <5510613317@student.tu.ac.th>
2015-03-26 15:49:14 +0700

    Update 15.42

---
+++ Reservation System/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reservation_System
+{
+    static class Program
+    {
+        public static bool session;
+        public static string username;
+        [STAThread]
+        static void Main(String[]args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            session = true;
+           
+             while (true)
+             {
+                 session = true;
+                 MainForm loginForm = new MainForm();
+                 Application.Run(loginForm);
+                 if (session)
+                {
+                    Clock clock = new Clock();
+                    Application.Run(clock);
+               } 
+                    
+             } 
+        }
+    }
+}
